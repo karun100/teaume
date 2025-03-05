1218a3d79a26d521b68895f1da47ddb03c0f1460
karun100 <kparuthi@juniper.net>
2025-03-05 17:00:03 +0530

    firse

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -16,3 +16,35 @@ func (User) Fields() []ent.Field {
 func (User) Edges() []ent.Edge {
 	return nil
 }
+
+==============from yash======
+
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("full_name").NotEmpty(),
+		field.String("email").Unique().NotEmpty(),
+		field.String("username").Unique().NotEmpty(),
+		field.String("password").Sensitive().NotEmpty(),
+		field.Enum("role").
+			Values("Seller", "User").
+			Default("User"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
